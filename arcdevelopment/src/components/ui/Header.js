b79d20280427bd775462c58f39ec89268c5a587b
muferro <muferro@gmail.com>
2020-12-26 19:50:54 -0300

    useScrollTrigger component

--- arcdevelopment/src/components/ui/Header.js
+++ arcdevelopment/src/components/ui/Header.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import ToolBar from '@material-ui/core/Toolbar'
+import ToolBar from '@material-ui/core/Toolbar';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function ElevationScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0
+    });
+  }
+
 
 export default function Header(props){  
     return (
-        <AppBar>
-            <ToolBar>Arc Development</ToolBar>
-        </AppBar>
+        <ElevationScroll>
+            <AppBar>
+                <ToolBar>Arc Development</ToolBar>
+            </AppBar>
+        </ElevationScroll>
     );
 }
