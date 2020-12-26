604f56820257d1273950210f1cfa2265cb4fd647
muferro <muferro@gmail.com>
2020-12-26 19:24:14 -0300

    ToolBar component

--- arcdevelopment/src/components/App.js
+++ arcdevelopment/src/components/App.js
@@ -1,7 +1,10 @@
 
+import Header from '../components/ui/Header';
+
 function App() {
   return (
     <div className="App">
+      <Header />
     hello!
     </div>
   );
---
+++ arcdevelopment/src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/Toolbar'
+
+export default function Header(props){  
+    return (
+        <AppBar>
+            <ToolBar>Arc Development</ToolBar>
+        </AppBar>
+    );
+}
